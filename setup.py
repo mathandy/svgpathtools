46dffa3aa51b6645d67fc4216866bd55a95eabc0
Andy <andyaport@gmail.com>
2016-07-05 23:02:28 -0700

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-VERSION = '1.0'
+VERSION = '1.0.1'
 AUTHOR_NAME = 'Andy Port'
 AUTHOR_EMAIL = 'AndyAPort@gmail.com'
 
@@ -9,11 +9,11 @@ setup(name='svgpathtools',
       version=VERSION,
       description=('A collection of tools for manipulating and analyzing SVG '
                    'Path objects and Bezier curves.'),
-      long_description=open('README.rst').read(),
+      # long_description=open('README.rst').read(),
       author=AUTHOR_NAME,
       author_email=AUTHOR_EMAIL,
       url='https://github.com/mathandy/svgpathtools',
-      download_url = 'http://github.com/mathandy/svgpathtools/tarball/1.0',
+      download_url = 'http://github.com/mathandy/svgpathtools/tarball/' + VERSION,
       license='MIT',
       
       # install_requires=['numpy', 'svgwrite'],
