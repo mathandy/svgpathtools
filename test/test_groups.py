3512f869680c3aaf3b12c03355374e2c0be87f4e
Michael X. Grey <grey@osrfoundation.org>
2018-05-09 17:39:54 -0700

    Beginning to write tests for groups

---
+++ test/test_groups.py
@@ -0,0 +1,8 @@
+from __future__ import division, absolute_import, print_function
+import unittest
+from svgpathtools import *
+
+class TestGroups(unittest.TestCase):
+
+    def test_group_transform(self):
+        pass
