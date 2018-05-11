1c8ca10f73e242a4c31bfdab3152c2ebddb01968
Michael X. Grey <grey@osrfoundation.org>
2018-05-10 17:06:16 -0700

    Print out some paths to see that they're sane

--- test/test_groups.py
+++ test/test_groups.py
@@ -10,5 +10,10 @@ class TestGroups(unittest.TestCase):
         doc = Document(join(dirname(__file__), 'groups.svg'))
 
         result = doc.flatten_all_paths()
-        print('\nnumber of paths: '+str(len(result)))
+        print('\nNumber of paths: '+str(len(result)))
         self.assertGreater(len(result), 0)
+
+        for svg_path in result:
+            print(svg_path.path)
+
+        # TODO: Test that the paths were transformed as expected
