0fdaad0284f537a15ac9f008042ee0fd465b2c3a
Michael X. Grey <grey@openrobotics.org>
2018-05-29 12:54:20 +0800

    Remove unnecessary import

--- svgpathtools/svg2paths.py
+++ svgpathtools/svg2paths.py
@@ -6,7 +6,6 @@ from __future__ import division, absolute_import, print_function
 from xml.dom.minidom import parse
 from os import path as os_path, getcwd
 import re
-import xml.etree.cElementTree as etree
 
 # Internal dependencies
 from .parser import parse_path
