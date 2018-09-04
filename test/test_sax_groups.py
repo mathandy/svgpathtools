42ff68f24a25554abead80c592d6ac4f1b912e50
tatarize <tatarize@gmail.com>
2018-09-03 19:31:10 -0700

    Test Update for None Matrix

--- test/test_sax_groups.py
+++ test/test_sax_groups.py
@@ -16,17 +16,15 @@ class TestSaxGroups(unittest.TestCase):
     def test_parse_display(self):
         doc = SaxDocument(join(dirname(__file__), 'transforms.svg'))
         # doc.display()
-        identity = np.identity(3)
         for i, node in enumerate(doc.tree):
             values = node
             path_value = values['d']
             matrix = values['matrix']
             self.assertTrue(values is not None)
             self.assertTrue(path_value is not None)
-            self.assertTrue(matrix is not None)
             if i == 0:
                 self.assertEqual(values['fill'], 'red')
             if i == 8 or i == 7:
-                self.assertTrue(np.all(np.equal(matrix, identity)))
+                self.assertEqual(matrix, None)
             if i == 9:
                 self.assertEqual(values['fill'], 'lime')
