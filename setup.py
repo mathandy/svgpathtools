96676b7697404de61ab054f436f83c194b1a28d8
Andrew Port <andyaport@gmail.com>
2023-02-13 17:46:17 -0500

    increment version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import codecs
 import os
 
 
-VERSION = '1.5.1'
+VERSION = '1.6.0'
 AUTHOR_NAME = 'Andy Port'
 AUTHOR_EMAIL = 'AndyAPort@gmail.com'
 GITHUB = 'https://github.com/mathandy/svgpathtools'
