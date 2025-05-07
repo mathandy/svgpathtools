e3eee5bf8870f40aaa1f2b8075943ed0f0b69bbd
Andrew Port <andyaport@gmail.com>
2025-05-07 18:22:03 -0400

    fix test_path_parsing to be platform agnostic

--- test/test_generation.py
+++ test/test_generation.py
@@ -2,8 +2,29 @@
 #------------------------------------------------------------------------------
 from __future__ import division, absolute_import, print_function
 import unittest
+import re
+from typing import Optional
+
+import numpy as np
+
 from svgpathtools import parse_path
-import sys
+
+
+_space_or_comma_pattern = re.compile(r'[,\s]+')
+
+
+def _assert_d_strings_are_almost_equal(d1: str, d2: str, test_case=unittest.TestCase, msg: Optional[str] = None) -> bool:
+    """Slight differences are expected on different platforms, check each part is approx. as expected."""
+
+    parts1 = _space_or_comma_pattern.split(d1)
+    parts2 = _space_or_comma_pattern.split(d2)
+    test_case.assertEqual(len(parts1), len(parts2), msg=msg)
+    for p1, p2 in zip(parts1, parts2):
+        if p1.isalpha():
+            test_case.assertEqual(p1, p2, msg=msg)
+        else:
+            test_case.assertTrue(np.isclose(float(p1), float(p2)), msg=msg)
+
 
 
 class TestGeneration(unittest.TestCase):
@@ -25,7 +46,7 @@ class TestGeneration(unittest.TestCase):
             'M 200,300 Q 400,50 600,300 T 1000,300',
             'M -3.4E+38,3.4E+38 L -3.4E-38,3.4E-38',
             'M 0,0 L 50,20 M 50,20 L 200,100 Z',
-            # 'M 600,350 L 650,325 A 25,25 -30 0,1 700,300 L 750,275',
+            'M 600,350 L 650,325 A 25,25 -30 0,1 700,300 L 750,275',
         ]
         float_paths = [
             'M 100.0,100.0 L 300.0,100.0 L 200.0,300.0 L 100.0,100.0',
@@ -42,32 +63,16 @@ class TestGeneration(unittest.TestCase):
             'M 200.0,300.0 Q 400.0,50.0 600.0,300.0 Q 800.0,550.0 1000.0,300.0',
             'M -3.4e+38,3.4e+38 L -3.4e-38,3.4e-38',
             'M 0.0,0.0 L 50.0,20.0 L 200.0,100.0 L 50.0,20.0',
+            'M 600.0,350.0 L 650.0,325.0 A 27.9508497187,27.9508497187 -30.0 0,1 700.0,300.0 L 750.0,275.0'
         ]
 
-        # if sys.version_info < (3,):
-        #     float_paths.append('M 600.0,350.0 L 650.0,325.0 A 27.9508497187,27.9508497187 -30.0 0,1 700.0,300.0 L 750.0,275.0')
-        # elif sys.version_info < (3, 10):
-        #     float_paths.append('M 600.0,350.0 L 650.0,325.0 A 27.95084971874737,27.95084971874737 -30.0 0,1 700.0,300.0 L 750.0,275.0')
-        # else:
-        #     float_paths.append('M 600.0,350.0 L 650.0,325.0 A 27.950849718747367,27.950849718747367 -30.0 0,1 700.0,300.0 L 750.0,275.0')
-
-        for path, flpath in zip(paths[::-1], float_paths[::-1]):
-            # Note:  Python 3 and Python 2 differ in the number of digits
-            # truncated when returning a string representation of a float
+        for path, expected in zip(paths, float_paths):
             parsed_path = parse_path(path)
             res = parsed_path.d()
-            option1 = res == path
-            option2 = res == flpath
+            msg = ('\npath =\n {}\nexpected =\n {}\nparse_path(path).d() =\n {}'
+                   ''.format(path, expected, res))
+            _assert_d_strings_are_almost_equal(res, expected, self, msg)
 
-            msg = ('\npath =\n {}\nflpath =\n {}\nparse_path(path).d() =\n {}'
-                   ''.format(path, flpath, res))
-            self.assertTrue(option1 or option2, msg=msg)
-
-        for flpath in float_paths[:-1]:
-            res = parse_path(flpath).d()
-            msg = ('\nflpath =\n {}\nparse_path(path).d() =\n {}'
-                   ''.format(flpath, res))
-            self.assertTrue(res == flpath, msg=msg)
 
     def test_normalizing(self):
         # Relative paths will be made absolute, subpaths merged if they can,
